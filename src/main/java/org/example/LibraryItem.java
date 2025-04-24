614ff4b1b31c171ed342693aac3a8ff6d4b3c2f6
Gavin Hensley <gavinhensley@protonmail.com>
2025-04-24 13:45:11 -0400

    Set up library item to be the super class

---
+++ src/main/java/org/example/LibraryItem.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class LibraryItem {
+
+    private final String title;
+    private final String id;
+    private boolean isAvailable;
+
+    public LibraryItem(String title, String id) {
+        this.title = title;
+        this.id = id;
+        this.isAvailable = true;
+    }
+
+    public String getTitle() {return title;}
+    public String getId() {return id;}
+    public boolean isAvailable() {return isAvailable;}
+
+    protected void setAvailable(boolean available) {this.isAvailable = available;}
+
+}
